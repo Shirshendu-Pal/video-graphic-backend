30167232309d58ae58493ae63343dff5de1ab18a
Shirshendu-Pal <shirshendupal9@gmail.com>
2024-08-11 01:12:58 +0530

    changes

--- src/services/email-config.js
+++ src/services/email-config.js
@@ -1,7 +1,7 @@
 const Sib = require("sib-api-v3-sdk");
 const config = require('../configuration/config');
 
-console.log(config.mail.apiKey)
+// console.log(config.mail.apiKey)
 module.exports = () => {
 const client = Sib.ApiClient.instance
 
