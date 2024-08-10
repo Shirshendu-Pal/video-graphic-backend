4e9167489cf584a5782d6aaa441f9bccafe5730d
Shirshendu-Pal <shirshendupal9@gmail.com>
2024-08-11 00:10:13 +0530

    first

--- src/services/email-config.js
+++ src/services/email-config.js
@@ -5,7 +5,7 @@ module.exports = () => {
 const client = Sib.ApiClient.instance
 
 const apiKey = client.authentications["api-key"]
-apiKey.apiKey =  "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"
+
 const tranEmailApi = new Sib.TransactionalEmailsApi()
 
 return tranEmailApi
